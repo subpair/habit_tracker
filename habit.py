228d144d9e3170c1cbd4e91f9087fc83520548a7
subpair <michael.reichel1@iu-study.org>
2022-10-14 03:00:35 +0200

    rebase

--- habit.py
+++ habit.py
@@ -1,60 +1,103 @@
-from db import Database
 from datetime import date, timedelta, datetime
+from db import Database
 
 
 class Habit:
-    def __init__(self, name: str, description: str = None, periodicity: int = None, default_time: int = None,
-                 db_filename: str = None, user_mode: bool = None):
+    def __init__(self, name: str = None, description: str = None, periodicity: int = None, default_time: int = None,
+                 db_filename: str = None, user_mode: bool = None) -> None:
 
-        self.name = name
-        self.description = description
-        self.periodicity = periodicity
-        self.date_today = date.today()
-        self.unique_id = 0
-        self.next_periodicity_due_date = date.today()
-        self.next_periodicity_range_start = date.today()
-        self.date_format = "%Y-%m-%d"
+        if name is None:
+            self.name: str = ""
+        else:
+            self.name = name
+
+        if description is None:
+            self.description: str = ""
+        else:
+            self.description = description
+
+        if periodicity is None:
+            self.periodicity: int = 0
+        else:
+            self.periodicity = periodicity
+
         if default_time is None:
-            self.default_time = 0
+            self.default_time: int = 0
         else:
             self.default_time = default_time
 
-        self.time = 0
-
         if db_filename is None:
-            self.db_filename = "main.db"
+            self.db_filename: str = "main.db"
         else:
             self.db_filename = db_filename
-        self.database = Database(self.db_filename)
 
         if user_mode is None:
-            self.user_mode = True
+            self.user_mode: bool = True
         else:
             self.user_mode = user_mode
 
-    def initialize_database(self):
+        self.completed: bool = False
+        self.database: Database = Database(self.db_filename)
+
+        self.time: int = 0
+        self.unique_id: int = 0
+
+        self.date_format: str = "%Y-%m-%d"
+        self.date_today: date = date.today()
+        self.next_periodicity_due_date: date = date.today()
+        self.next_periodicity_range_start: date = date.today()
+        self.initialize_database()
+
+    def initialize_database(self) -> None:
         self.database.initialize_database()
 
-    def is_existing(self, habit_name: str):
-        if self.database.select_get_habit_unique_id(habit_name) is not None:
+    def is_existing(self, habit_name: str) -> bool:
+        if self.database.read_habit_unique_id(habit_name) is not None:
             return True
         else:
             return False
 
-    def set_id(self, habit_name: str):
-        self.unique_id = self.database.select_get_habit_unique_id(habit_name)[0]
+    def set_id(self, habit_name: str) -> bool:
+        unique_id: tuple = self.database.read_habit_unique_id(habit_name)
+        if unique_id is not None:
+            self.unique_id = unique_id[0]
+            return True
+        else:
+            return False
 
-    def set_periodicity(self, habit_id: int):
-        self.periodicity = self.database.select_periodicity(habit_id)[0]
+    def set_periodicity(self, habit_id: int) -> bool:
+        periodicity: tuple = self.database.read_habit_periodicity(habit_id)
+        if periodicity is not None:
+            self.periodicity = periodicity[0]
+            return True
+        else:
+            return False
 
-    def set_next_periodicity_due_date(self, habit_id: int):
-        next_periodicity_due_date = self.database.select_next_periodicity_due_date(habit_id)[0]
-        self.next_periodicity_due_date = datetime.strptime(next_periodicity_due_date, self.date_format).date()
+    def set_next_periodicity_due_date(self, habit_id: int) -> bool:
+        next_periodicity_due_date: tuple = self.database.read_next_periodicity_due_date(habit_id)
+        if next_periodicity_due_date is not None:
+            self.next_periodicity_due_date = datetime.strptime(next_periodicity_due_date[0], self.date_format).date()
+            return True
+        else:
+            return False
 
-    def set_default_time(self, habit_id: int):
-        self.default_time = self.database.select_get_habit_default_time(habit_id)[0]
+    def set_default_time(self, habit_id: int) -> bool:
+        default_time: tuple = self.database.read_habit_default_time(habit_id)
+        if default_time is not None:
+            self.default_time = default_time[0]
+            return True
+        else:
+            return False
 
-    def create_habit(self, created_date: date = None, next_periodicity_due_date: date = None):
+    def set_name(self, habit_id : int) -> bool:
+        name: tuple = self.database.read_habit_name(habit_id)
+        if name is not None:
+            self.name = name[0]
+            return True
+        else:
+            return False
+
+    def create_habit(self, created_date: date = None, next_periodicity_due_date: date = None) -> bool:
         if created_date is None:
             if self.user_mode:
                 created_date = date.today()
@@ -62,101 +105,94 @@ class Habit:
                 created_date = self.date_today
         if next_periodicity_due_date is None:
             next_periodicity_due_date = created_date + timedelta(days=self.periodicity)
-        self.database.insert_habit(self.name, self.description, self.periodicity,
-                                   created_date, next_periodicity_due_date, self.default_time)
-        self.set_id(self.name)
+        create = self.database.create_habit(self.name, self.description, self.periodicity,
+                                            created_date, next_periodicity_due_date, self.default_time)
+        if create is not None:
+            self.set_id(self.name)
+        return create
 
-    def create_event(self, completed: bool, next_periodicity_due_date: date, change_date: date = None,
-                     time: int = None):
+    def create_event(self, completed: bool, next_periodicity_due_date: date, change_date: date = None) -> bool:
         if change_date is None:
             if self.user_mode:
                 change_date = date.today()
             else:
                 change_date = self.date_today
+        if self.time == 0:
+            self.time = self.default_time
         if not completed:
             time = 0
-        if time is None:
-            time = self.default_time
-        self.database.insert_event(self.unique_id, completed, change_date, time)
-        self.next_periodicity_due_date = next_periodicity_due_date + timedelta(days=self.periodicity)
-        self.database.update_next_periodicity_due_date(self.unique_id, self.next_periodicity_due_date)
+        else:
+            time = self.time
+        create = self.database.create_event(self.unique_id, completed, change_date, time)
+        if create is not None:
+            self.next_periodicity_due_date = next_periodicity_due_date + timedelta(days=self.periodicity)
+            self.database.update_next_periodicity_due_date(self.unique_id, self.next_periodicity_due_date)
+        return create
 
-    def create_event_logic(self, completed: bool, next_periodicity_due_date: date, change_date: date = None):
+    def create_event_logic(self, next_periodicity_due_date: date, change_date: date = None) \
+            -> tuple[str, dict]:
+        status: str = ""
+        missed_dates: dict = {}
         if change_date is None:
             if self.user_mode:
                 change_date = date.today()
             else:
                 change_date = self.date_today
-
-        update_lower_range = next_periodicity_due_date - timedelta(days=self.periodicity)
+        update_lower_range: date = next_periodicity_due_date - timedelta(days=self.periodicity)
+        # print(update_lower_range)
+        # print(change_date)
+        # print(next_periodicity_due_date)
         if (change_date >= update_lower_range) and (change_date <= next_periodicity_due_date):
-            self.create_event(completed, self.next_periodicity_due_date, change_date=change_date)
+            self.create_event(self.completed, self.next_periodicity_due_date, change_date=change_date)
+            status = "normal"
+            missed_dates[0] = change_date
         elif change_date < update_lower_range:
-            return False
+            status = "too early"
+            missed_dates[0] = update_lower_range
         elif change_date > next_periodicity_due_date:
+            status = "with fill"
             update_lower_range, missed_dates = self.create_event_fill(update_lower_range)
-            self.create_event(completed, self.next_periodicity_due_date, update_lower_range)
-            return missed_dates
+            self.create_event(self.completed, self.next_periodicity_due_date, update_lower_range)
+            missed_dates[0] = update_lower_range
+        return status, missed_dates
 
-    def create_event_fill(self, update_lower_range: date):
-        missed = int(((self.date_today - timedelta(days=self.periodicity)) - update_lower_range)
-                     / timedelta(days=self.periodicity))
-        missed_dates = {}
+    def create_event_fill(self, update_lower_range: date) -> tuple[date, dict]:
+        missed: int = int(((self.date_today - timedelta(days=self.periodicity)) - update_lower_range)
+                          / timedelta(days=self.periodicity))
+        missed_dates: dict = {}
         for i in range(missed):
-            missed_dates[i+1] = str(update_lower_range)
+            missed_dates[i + 1] = str(update_lower_range)
             self.create_event(False, self.next_periodicity_due_date, update_lower_range)
             self.database.update_next_periodicity_due_date(self.unique_id, self.next_periodicity_due_date)
             update_lower_range = self.next_periodicity_due_date - timedelta(days=self.periodicity)
         return update_lower_range, missed_dates
 
-    def remove(self, habit_id: int):
-        self.database.delete_habit_and_events(habit_id)
+    def delete(self, habit_id: int) -> bool:
+        delete = self.database.delete_habit_and_events(habit_id)
+        return delete
 
-    def analyse(self, option: str):
-        """
-        The analyse-function will use the data from the database and give the user information about the existing data
-        of the habits.\n
-        :param option: can be "all", "all same periodicity", "longest streak of all", "longest streak" or "time"
-        :return: always True
-        """
-        # "Show all currently tracked habits"
-        if option == "all":
-            self.analyse_all_active()
-        # "Show all habits with the same periodicity"
-        elif option == "all same periodicity":
-            self.analyse_all_active_same_periodicity(self.periodicity)
-        # "Return the longest run streak of all defined habits"
-        elif option == "longest streak of all":
-            self.analyse_longest_streak()
-        # "Return the longest run streak for a given habit"
-        elif option == "longest streak":
-            self.analyse_longest_streak(self.unique_id)
-        # "Return the longest run streak for a given habit"
-        elif option == "time":
-            self.analyse_time(self.unique_id)
-
-    def analyse_all_active(self):
-        result = self.database.analyse_get_all()
+    def analyse_all_active(self) -> list:
+        result: list = self.database.read_habits_by_finished()
         return result
 
-    def analyse_all_active_same_periodicity(self, periodicity: int):
-        result = self.database.analyse_get_same_periodicity(periodicity)
+    def analyse_all_active_same_periodicity(self, periodicity: int) -> list:
+        result: list = self.database.read_habits_by_periodicity(periodicity)
         return result
 
-    def analyse_longest_streak(self, habit_id: int = None):
+    def analyse_longest_streak(self, habit_id: int = None) -> tuple:
         if habit_id is None:
-            habit_unique_ids = self.database.select_get_all_habits_unique_ids()
+            habit_unique_ids: list = self.database.read_habits_unique_ids()
         else:
             habit_unique_ids = [(habit_id,)]
         if habit_unique_ids:
             # Count the number of times an event was successful for a habit by iterating over all events in all
             # existing habits.
-            highest_count_overall = 0
-            highest_habit_id = 0
+            highest_count_overall: int = 0
+            highest_habit_id: int = 0
             for i in habit_unique_ids:
-                all_events = self.database.select_get_habit_events(unique_id=i[0])
-                count = 0
-                highest_count = 0
+                all_events = self.database.read_habit_events(unique_id=i[0])
+                count: int = 0
+                highest_count: int = 0
                 for j in all_events:
                     if j[2] == 1:
                         count += 1
@@ -173,12 +209,12 @@ class Habit:
                     highest_habit_id = i[0]
             return highest_habit_id, highest_count_overall
         else:
-            return None
+            return ()
 
-    def analyse_time(self, habit_id: int):
-        all_events = self.database.select_get_habit_events(habit_id)
+    def analyse_time(self, habit_id: int) -> int:
+        all_events: list = self.database.read_habit_events(habit_id)
         if all_events:
-            time_summary = 0
+            time_summary: int = 0
             for i in all_events:
                 if i[2] == 1:
                     time_summary += i[3]
@@ -190,11 +226,11 @@ class Habit:
 
     # Methods currently only used in unit testing
 
-    def get_event_count(self, habit_id: int):
-        habit_events = self.database.select_get_habit_events(habit_id)
+    def get_event_count(self, habit_id: int) -> int:
+        habit_events: list = self.database.read_habit_events(habit_id)
         return len(habit_events)
 
-    def manipulate_time(self, offset: int):
+    def manipulate_time(self, offset: int) -> None:
         """
         Currently only used in unit testing to go back in time\n
         :param offset: can be either a positive or negative number
--- sample_data.py
+++ sample_data.py
@@ -3,7 +3,7 @@ from habit import Habit
 
 
 class SampleData:
-    def __init__(self, duration: int):
+    def __init__(self, duration: int) -> None:
         """
         Initialize the sampledata object with a duration value for which the simulated events take place \n
         Steps:\n
@@ -12,70 +12,71 @@ class SampleData:
         :param duration: time in days of which the sample data is offset
         """
 
-        self.duration = duration
-        # 1 set database filename
-        self.db_filename = "sample.db"
+        self.duration: int = duration
+        # 1 set database filename and initialize database
+        self.db_filename: str = "sample.db"
 
-        # 2 initialize habit objects
-        self.habit_one = Habit(self.db_filename)
-        self.habit_two = Habit(self.db_filename)
-        self.habit_three = Habit(self.db_filename)
-        self.habit_four = Habit(self.db_filename)
-        self.habit_five = Habit(self.db_filename)
 
-    def create_habits(self):
+    def create_habits(self) -> None:
         """
         Steps:\n
         1: Set the values name, description, periodicity and default_time_value for the 5 predefined habits \n
         2: Negate the duration value of the simulation, so the sample data becomes usable today\n
         3: Store the habits in the database
         """
+        self.habit = Habit(db_filename=self.db_filename)
+        self.habit.initialize_database()
+        self.habit.database.close_connection()
+
+        # 2 initialize habit objects
+        self.habit_one = Habit(db_filename=self.db_filename)
+        self.habit_two = Habit(db_filename=self.db_filename)
+        self.habit_three = Habit(db_filename=self.db_filename)
+        self.habit_four = Habit(db_filename=self.db_filename)
+        self.habit_five = Habit(db_filename=self.db_filename)
 
         # 1 assigning the habit definitions and manipulating the time value
         self.habit_one.name = "practice guitar"
         self.habit_one.description = "for at least 30min"
-        self.habit_one.periodicity = "daily"
-        self.habit_one.default_time_value = 30
+        self.habit_one.periodicity = 1
+        self.habit_one.default_time = 30
+        self.habit_one.user_mode = False
         self.habit_one.manipulate_time(-self.duration)
 
         self.habit_two.name = "sleep 6 hours"
         self.habit_two.description = "at least 6 hours per day"
-        self.habit_two.periodicity = "daily"
-        self.habit_two.default_time_value = 360
+        self.habit_two.periodicity = 1
+        self.habit_two.default_time = 360
+        self.habit_two.user_mode = False
         self.habit_two.manipulate_time(-self.duration)
 
         self.habit_three.name = "read a book"
         self.habit_three.description = "every week a little bit"
-        self.habit_three.periodicity = "weekly"
-        self.habit_three.default_time_value = 0
+        self.habit_three.periodicity = 7
+        self.habit_three.default_time = 0
+        self.habit_three.user_mode = False
         self.habit_three.manipulate_time(-self.duration)
 
         self.habit_four.name = "do code challenges"
         self.habit_four.description = "at least 30 min"
-        self.habit_four.periodicity = "daily"
-        self.habit_four.default_time_value = 30
+        self.habit_four.periodicity = 1
+        self.habit_four.default_time = 30
+        self.habit_four.user_mode = False
         self.habit_four.manipulate_time(-self.duration)
 
         self.habit_five.name = "study daily"
         self.habit_five.description = "without interruptions"
-        self.habit_five.periodicity = "daily"
-        self.habit_five.default_time_value = 120
+        self.habit_five.periodicity = 1
+        self.habit_five.default_time = 120
+        self.habit_five.user_mode = False
         self.habit_five.manipulate_time(-self.duration)
 
         # 2 storing the habits in the database
-        self.habit_one.create(self.habit_one.name, self.habit_one.description, self.habit_one.periodicity,
-                              self.habit_one.default_time_value)
-
-        self.habit_two.create(self.habit_two.name, self.habit_two.description, self.habit_two.periodicity,
-                              self.habit_two.default_time_value)
-
-        self.habit_three.create(self.habit_three.name, self.habit_three.description, self.habit_three.periodicity,
-                                self.habit_three.default_time_value)
-        self.habit_four.create(self.habit_four.name, self.habit_four.description, self.habit_four.periodicity,
-                               self.habit_four.default_time_value)
-
-        self.habit_five.create(self.habit_five.name, self.habit_five.description, self.habit_five.periodicity,
-                               self.habit_five.default_time_value)
+        self.habit_one.create_habit()
+        self.habit_two.create_habit()
+        self.habit_three.create_habit()
+        self.habit_four.create_habit()
+        self.habit_five.create_habit()
 
     def simulate_events(self):
         """
@@ -87,93 +88,118 @@ class SampleData:
 
         # "practice guitar"
         days = self.duration
-        print("simulating events for {days} days".format(days=days))
+        #print("simulating events for {days} days".format(days=days))
         for i in range(days):
             self.habit_one.manipulate_time(+1)
             answers = [True, False]
-            completion = choices(answers, weights=(25, 75))[0]
+            self.habit_one.completed = choices(answers, weights=(25, 75))[0]
             use_time = choices(answers, weights=(90, 10))[0]
             if use_time:
-                time_invested = randrange(0, 240)
+                self.habit_one.time = randrange(0, 240)
             else:
-                time_invested = 0
+                self.habit_one.time = 0
             skip_habit = choices(answers, weights=(50, 50))[0]
             if not skip_habit:
-                self.habit_one.event(self.habit_one.name, completion, time_invested)
+                self.habit_one.set_id(self.habit_one.name)
+                self.habit_one.set_periodicity(self.habit_one.unique_id)
+                self.habit_one.set_default_time(self.habit_one.unique_id)
+                self.habit_one.set_next_periodicity_due_date(self.habit_one.unique_id)
+                self.habit_one.create_event_logic(self.habit_one.next_periodicity_due_date)
             else:
-                print("skipping")
+                pass
+                # print("skipping")
 
         # "sleep 6 hours"
         days = self.duration
-        print("simulating events for {days} days".format(days=days))
+        #print("simulating events for {days} days".format(days=days))
         for i in range(days):
             self.habit_two.manipulate_time(+1)
             answers = [True, False]
-            completion = choices(answers, weights=(95, 5))[0]
+            self.habit_two.completed = choices(answers, weights=(95, 5))[0]
             use_time = choices(answers, weights=(25, 75))[0]
             if use_time:
-                time_invested = randrange(0, 720)
+                self.habit_two.time = randrange(0, 720)
             else:
-                time_invested = 0
+                self.habit_two.time = 0
             skip_habit = choices(answers, weights=(2, 98))[0]
             if not skip_habit:
-                self.habit_two.event(self.habit_two.name, completion, time_invested)
+                self.habit_two.set_id(self.habit_two.name)
+                self.habit_two.set_periodicity(self.habit_two.unique_id)
+                self.habit_two.set_default_time(self.habit_two.unique_id)
+                self.habit_two.set_next_periodicity_due_date(self.habit_two.unique_id)
+                self.habit_two.create_event_logic(self.habit_two.next_periodicity_due_date)
             else:
-                print("skipping")
+                pass
+                # print("skipping")
 
         # "read a book"
         days = self.duration
-        print("simulating events for {days} days".format(days=days))
+        #print("simulating events for {days} days".format(days=days))
         for i in range(days):
             self.habit_three.manipulate_time(+1)
             answers = [True, False]
-            completion = choices(answers, weights=(10, 90))[0]
+            self.habit_three.completed = choices(answers, weights=(10, 90))[0]
             use_time = choices(answers, weights=(25, 75))[0]
             if use_time:
-                time_invested = randrange(0, 120)
+                self.habit_three.time = randrange(0, 120)
             else:
-                time_invested = 0
+                self.habit_three.time = 0
             skip_habit = choices(answers, weights=(90, 10))[0]
             if not skip_habit:
-                self.habit_three.event(self.habit_three.name, completion, time_invested)
+                self.habit_three.set_id(self.habit_three.name)
+                self.habit_three.set_periodicity(self.habit_three.unique_id)
+                self.habit_three.set_default_time(self.habit_three.unique_id)
+                self.habit_three.set_next_periodicity_due_date(self.habit_three.unique_id)
+                self.habit_three.create_event_logic(self.habit_three.next_periodicity_due_date)
             else:
-                print("skipping")
+                pass
+                # print("skipping")
 
         # "do code challenges"
         days = self.duration
-        print("simulating events for {days} days".format(days=days))
+        #print("simulating events for {days} days".format(days=days))
         for i in range(days):
             self.habit_four.manipulate_time(+1)
             answers = [True, False]
-            completion = choices(answers, weights=(75, 25))[0]
+            self.habit_four.completed = choices(answers, weights=(75, 25))[0]
             use_time = choices(answers, weights=(75, 25))[0]
             if use_time:
-                time_invested = randrange(0, 180)
+                self.habit_four.time = randrange(0, 180)
             else:
-                time_invested = 0
+                self.habit_four.time = 0
             skip_habit = choices(answers, weights=(40, 60))[0]
             if not skip_habit:
-                self.habit_four.event(self.habit_four.name, completion, time_invested)
+                self.habit_four.set_id(self.habit_four.name)
+                self.habit_four.set_periodicity(self.habit_four.unique_id)
+                self.habit_four.set_default_time(self.habit_four.unique_id)
+                self.habit_four.set_next_periodicity_due_date(self.habit_four.unique_id)
+                self.habit_four.create_event_logic(self.habit_four.next_periodicity_due_date)
             else:
-                print("skipping")
+                pass
+                # print("skipping")
 
         # "study daily"
         days = self.duration
-        print("simulating events for {days} days".format(days=days))
+        #print("simulating events for {days} days".format(days=days))
         for i in range(days):
             self.habit_five.manipulate_time(+1)
             answers = [True, False]
-            completion = choices(answers, weights=(99, 1))[0]
+            self.habit_five.completed = choices(answers, weights=(99, 1))[0]
             use_time = choices(answers, weights=(95, 5))[0]
             if use_time:
-                time_invested = randrange(120, 480)
+                self.habit_five.time = randrange(120, 480)
             else:
-                time_invested = 0
+                self.habit_five.time = 0
             skip_habit = choices(answers, weights=(1, 99))[0]
             if not skip_habit:
-                self.habit_five.event(self.habit_five.name, completion, time_invested)
+                self.habit_five.set_id(self.habit_five.name)
+                self.habit_five.set_periodicity(self.habit_five.unique_id)
+                self.habit_five.set_default_time(self.habit_five.unique_id)
+                self.habit_five.set_next_periodicity_due_date(self.habit_five.unique_id)
+                self.habit_five.create_event_logic(self.habit_five.next_periodicity_due_date)
             else:
-                print("skipping")
+                pass
+                # print("skipping")
 
     def closing_connections(self):
         """
@@ -185,5 +211,3 @@ class SampleData:
         self.habit_three.database.close_connection()
         self.habit_four.database.close_connection()
         self.habit_five.database.close_connection()
-
-
